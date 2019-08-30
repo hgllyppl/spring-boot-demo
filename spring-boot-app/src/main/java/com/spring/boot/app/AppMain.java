d12f82336bb712609372cd3100bbb6a392143947
lixin <hgllyppl@yeah.net>
2019-08-30 11:49:54 +0800

    [update] 修改 boot-app 注释

--- spring-boot-app/src/main/java/com/spring/boot/app/AppMain.java
+++ spring-boot-app/src/main/java/com/spring/boot/app/AppMain.java
@@ -1,7 +1,10 @@
 package com.spring.boot.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationImportSelector;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.config.ConfigFileApplicationListener;
+import org.springframework.boot.context.logging.LoggingApplicationListener;
 
 /**
  * Created by xin on 2019/4/28.
@@ -9,9 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppMain {
 
-    // 如何确定环境配置
-    // 如何统一日志
-    // 自动配置-条件
+    /**
+     * 如何确定环境配置
+     * 如何统一日志
+     * 自动配置-过滤
+     * @see ConfigFileApplicationListener
+     * @see LoggingApplicationListener
+     * @see AutoConfigurationImportSelector#filter
+     */
     public static void main(String[] args) {
         SpringApplication.run(AppMain.class, args);
     }
