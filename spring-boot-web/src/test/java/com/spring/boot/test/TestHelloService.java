de3cb79ac177ef338639754bccc05639c377b156
lixin <hgllyppl@yeah.net>
2019-09-17 17:35:54 +0800

    [update] 修改测试用例

--- spring-boot-web/src/test/java/com/spring/boot/test/TestHelloService.java
+++ spring-boot-web/src/test/java/com/spring/boot/test/TestHelloService.java
@@ -1,24 +1,37 @@
 package com.spring.boot.test;
 
-import com.spring.boot.service.HelloService;
+import com.spring.boot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by xin on 2019/9/16.
  */
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = TestServiceConf.class)
+@WebMvcTest(HelloController.class)
+@TestServiceConf
 public class TestHelloService {
 
     @Autowired
-    private HelloService helloService;
+    private MockMvc mockMvc;
+
+//    @MockBean
+//    private HelloService helloService;
 
     @Test
-    public void test() {
-        helloService.hello("z3");
+    public void test() throws Exception {
+        mockMvc.perform(post("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn()
+        ;
     }
 }
--- spring-boot-web/src/test/java/com/spring/boot/test/TestServiceConf.java
+++ spring-boot-web/src/test/java/com/spring/boot/test/TestServiceConf.java
@@ -1,12 +1,27 @@
 package com.spring.boot.test;
 
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Created by xin on 2019/9/16.
  */
-@Configuration
-@ComponentScan({"com.spring.boot.service", "com.spring.boot.dao"})
-public class TestServiceConf {
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ContextConfiguration(classes = TestServiceConf.Conf.class, initializers = ConfigFileApplicationContextInitializer.class)
+@ImportAutoConfiguration({})
+public @interface TestServiceConf {
+
+    @Configuration
+    @ComponentScan(value = {"com.spring.boot"}, lazyInit = true)
+    class Conf {}
 }
